39c881fb6be5b3bf3baabf381de36a1aa9947708
Srdjan Rados <srdjan.rados90@gmail.com>
2021-03-05 12:37:57 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c59d22af-8cde-4502-8d71-265e4b4e3822';
+export const MODAL_CLOSE_SEC = 2.5;
